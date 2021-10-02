03ec22a6a4ce492a93407162f36c2f54ad7e9d67
Tomé Luan <tomeluan05@gmail.com>
2021-10-02 20:35:12 -0300

    subindo projeto

---
+++ assets/scripts/script.js
@@ -0,0 +1,80 @@
+const FRONT = 'card_front';
+const BACK = 'card_back';
+const CARD = 'card';
+const ICON = 'icon';
+
+startGame();
+
+function startGame() {
+    initializeCards(game.createCardsFromTechs());
+}
+
+function initializeCards(cards) {
+    let gameBoard = document.getElementById("gameBoard");
+    gameBoard.innerHTML = '';
+    game.cards.forEach(card => {
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+
+        createCardContent(card, cardElement);
+
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);
+    })
+}
+
+function createCardContent(card, cardElement) {
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+}
+
+function createCardFace(face, card, element) {
+    //Criei o elemento cardElementFace
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+    cardElementFace.style.color="white";
+
+    if (face === FRONT) {
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src = './assets/images/' + card.icon + '.png';
+        cardElementFace.appendChild(iconElement);
+    } else {
+        //Aqui quero chamar a imagem para ela substituir o nome carta.
+        cardElementFace.innerHTML = '&lt/&gt';
+    }
+    element.appendChild(cardElementFace);
+}
+
+function flipCard() {
+    if (game.setCard(this.id)) {
+        this.classList.add('flip');
+        if (game.secondCard) {
+            if (game.checkMatch()) {
+                game.clearCards();
+                if(game.checkGameOver()){
+                    let gameOverLayer = document.getElementById('gameOver');
+                    gameOverLayer.style.display = 'flex';
+                }
+            } else {
+                setTimeout(() => {
+                    let firsCardView = document.getElementById(game.firstCard.id);
+                    let secondCardView = document.getElementById(game.secondCard.id);
+
+                    firsCardView.classList.remove('flip');
+                    secondCardView.classList.remove('flip');
+                    game.unflipCards();
+                }, 1000);
+            }
+        }
+    }
+}
+
+function restart(){
+    game.clearCards();
+    startGame();
+    let gameOverLayer = document.getElementById('gameOver');
+    gameOverLayer.style.display = 'none';
+}
